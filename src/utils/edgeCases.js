b661038d32031814c0e04ba80d100fabf3a6b437
Luke Bonaccorsi <luke@lukeb.co.uk>
2018-11-24 18:41:13 -0500

    Add buenos aires payment warning (#38)

--- src/utils/edgeCases.js
+++ src/utils/edgeCases.js
@@ -10,6 +10,12 @@ const cases = [
       'Be careful getting an Uber in porto because of taxi drivers. Mainly in airports and train stations',
     name: 'porto',
   },
+  {
+    type: 'payment',
+    message:
+      'Uber drivers only take payment by cash. If they think you\'ll pay by card then they\'ll cancel the ride',
+    name: 'buenos aires',
+  },
 ]
 
 export default city =>
