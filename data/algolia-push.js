db99601aaaac1f3aed6cadbd19faf3f0d494afe9
Sara Vieira <hey@iamsaravieira.com>
2018-12-03 18:39:03 +0100

    clear index before sending data

--- data/algolia-push.js
+++ data/algolia-push.js
@@ -5,7 +5,11 @@ const data = require('./utils/combine-files')
 let client = algoliasearch('UEHWANDHH2', process.env.ALGOLIA_KEY)
 let index = client.initIndex('cities')
 
-index.addObjects(data, (err, content) => {
-  if (err) console.log(err)
-  console.log(content)
+index.clearIndex(function(err, content) {
+  if (err) throw err
+
+  index.addObjects(data, (err, content) => {
+    if (err) console.log(err)
+    console.log(content)
+  })
 })
