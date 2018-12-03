9d3cbd967ab8e732c5f798f49301b04552391076
Maher Alkendi <maher.alkendi2@gmail.com>
2018-12-03 07:54:15 +0000

    added Careem (#67)
    * added Careem
    
    * change the npm script for seeding data. It now points to scrape.js

---
+++ data/single/careem.js
@@ -0,0 +1,442 @@
+module.exports = [
+  {
+    name: 'Dubai',
+    company: 'careem',
+    info: {
+      formatted_address: 'Dubai - United Arab Emirates',
+      country: {
+        long_name: 'United Arab Emirates',
+        short_name: 'AE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJRcbZaklDXz4RYlEphFBu5r0'
+    }
+  },
+  {
+    name: 'Abu Dhabi',
+    company: 'careem',
+    info: {
+      formatted_address: 'Abu Dhabi - United Arab Emirates',
+      country: {
+        long_name: 'United Arab Emirates',
+        short_name: 'AE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJufI-cg9EXj4RCBGXQZMuzMc'
+    }
+  },
+  {
+    name: 'Sharjah',
+    company: 'careem',
+    info: {
+      formatted_address: 'Sharjah - United Arab Emirates',
+      country: {
+        long_name: 'United Arab Emirates',
+        short_name: 'AE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJS5bn7V9fXz4RiW0fnKEKgyo'
+    }
+  },
+  {
+    name: 'Al Ain',
+    company: 'careem',
+    info: {
+      formatted_address: 'Al Ain - Abu Dhabi - United Arab Emirates',
+      country: {
+        long_name: 'United Arab Emirates',
+        short_name: 'AE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJSaDVy0Wxij4Rf5_yW-qMb_U'
+    }
+  },
+  {
+    name: 'Ras al Khaimah',
+    company: 'careem',
+    info: {
+      formatted_address: 'Ras al Khaimah - United Arab Emirates',
+      country: {
+        long_name: 'United Arab Emirates',
+        short_name: 'AE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJpwnSTA5x9j4RD-KEpgxnnrk'
+    }
+  },
+  {
+    name: 'Ajman',
+    company: 'careem',
+    info: {
+      formatted_address: 'Ajman - United Arab Emirates',
+      country: {
+        long_name: 'United Arab Emirates',
+        short_name: 'AE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJeb6P3WRXXz4RGahFZN6QoM0'
+    }
+  },
+  {
+    name: 'Doha',
+    company: 'careem',
+    info: {
+      formatted_address: 'Doha, Qatar',
+      country: {
+        long_name: 'Qatar',
+        short_name: 'QA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJf-jc_zTFRT4RsdTPeJ8x2UQ'
+    }
+  },
+  {
+    name: 'Riyadh',
+    company: 'careem',
+    info: {
+      formatted_address: 'Riyadh Saudi Arabia',
+      country: {
+        long_name: 'Saudi Arabia',
+        short_name: 'SA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJmZNIDYkDLz4R1Z_nmBxNl7o'
+    }
+  },
+  {
+    name: 'Dammam',
+    company: 'careem',
+    info: {
+      formatted_address: 'Dammam Saudi Arabia',
+      country: {
+        long_name: 'Saudi Arabia',
+        short_name: 'SA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJAzRrJzIdNj4RdlZeeuwB2e8'
+    }
+  },
+  {
+    name: 'Jeddah',
+    company: 'careem',
+    info: {
+      formatted_address: 'Jeddah Saudi Arabia',
+      country: {
+        long_name: 'Saudi Arabia',
+        short_name: 'SA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJWX4TsR_QwxUR2xixN5dXWeA'
+    }
+  },
+  {
+    name: 'Makkah',
+    company: 'careem',
+    info: {
+      formatted_address: 'Mecca Saudi Arabia',
+      country: {
+        long_name: 'Saudi Arabia',
+        short_name: 'SA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJdYeB7UwbwhURzslwz2kkq5g'
+    }
+  },
+  {
+    name: 'Manama',
+    company: 'careem',
+    info: {
+      formatted_address: 'Manama, Bahrain',
+      country: {
+        long_name: 'Bahrain',
+        short_name: 'BH',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJLaZ2J3KvST4RwmAPB6Y4Z4s'
+    }
+  },
+  {
+    name: 'Beirut',
+    company: 'careem',
+    info: {
+      formatted_address: 'Beirut, Lebanon',
+      country: {
+        long_name: 'Lebanon',
+        short_name: 'LB',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJj6eAWCEXHxURtDaY6bqCkXI'
+    }
+  },
+  {
+    name: 'Jounieh',
+    company: 'careem',
+    info: {
+      formatted_address: 'Jounieh, Lebanon',
+      country: {
+        long_name: 'Lebanon',
+        short_name: 'LB',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJuX5_JcRAHxURhKnn8u3UIgY'
+    }
+  },
+  {
+    name: 'Karachi',
+    company: 'careem',
+    info: {
+      formatted_address: 'Karachi, Karachi City, Sindh, Pakistan',
+      country: {
+        long_name: 'Pakistan',
+        short_name: 'PK',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJv0sdZQY-sz4RIwxaVUQv-Zw'
+    }
+  },
+  {
+    name: 'Lahore',
+    company: 'careem',
+    info: {
+      formatted_address: 'Lahore, Punjab, Pakistan',
+      country: {
+        long_name: 'Pakistan',
+        short_name: 'PK',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ2QeB5YMEGTkRYiR-zGy-OsI'
+    }
+  },
+  {
+    name: 'Islamabad',
+    company: 'careem',
+    info: {
+      formatted_address: 'Islamabad, Islamabad Capital Territory, Pakistan',
+      country: {
+        long_name: 'Pakistan',
+        short_name: 'PK',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJL3KReNC_3zgRtgLbO1xRWWA'
+    }
+  },
+  {
+    name: 'Kuwait',
+    company: 'careem',
+    info: {
+      formatted_address: 'Kuwait',
+      country: {
+        long_name: 'Kuwait',
+        short_name: 'KW',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJoVHqvj82xT8R0u3Yks1rcnQ'
+    }
+  },
+  {
+    name: 'Cairo',
+    company: 'careem',
+    info: {
+      formatted_address: 'Cairo, Cairo Governorate, Egypt',
+      country: {
+        long_name: 'Egypt',
+        short_name: 'EG',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ674hC6Y_WBQRujtC6Jay33k'
+    }
+  },
+  {
+    name: 'Alexandria',
+    company: 'careem',
+    info: {
+      formatted_address: 'Alexandria, Alexandria Governorate, Egypt',
+      country: {
+        long_name: 'Egypt',
+        short_name: 'EG',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ0w9xJpHE9RQRuWvuKabN4LQ'
+    }
+  },
+  {
+    name: 'Casablanca',
+    company: 'careem',
+    info: {
+      formatted_address: 'Casablanca, Morocco',
+      country: {
+        long_name: 'Morocco',
+        short_name: 'MA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJOxGqeEfNpw0R0w8xT9jBBgs'
+    }
+  },
+  {
+    name: 'Rabat',
+    company: 'careem',
+    info: {
+      formatted_address: 'Rabat, Morocco',
+      country: {
+        long_name: 'Morocco',
+        short_name: 'MA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJwcVQH4drpw0RawcIdO1GyXo'
+    }
+  },
+  {
+    name: 'Tangier',
+    company: 'careem',
+    info: {
+      formatted_address: 'Tangier, Morocco',
+      country: {
+        long_name: 'Morocco',
+        short_name: 'MA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJLRNM8FyHCw0ReuG0v3HFv3Y'
+    }
+  },
+  {
+    name: 'Amman',
+    company: 'careem',
+    info: {
+      formatted_address: 'Amman, Jordan',
+      country: {
+        long_name: 'Jordan',
+        short_name: 'JO',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJr4F5XbhfGxUR6GXc-MAwHGM'
+    }
+  },
+  {
+    name: 'Istanbul',
+    company: 'careem',
+    info: {
+      formatted_address: 'Istanbul, Turkey',
+      country: {
+        long_name: 'Turkey',
+        short_name: 'TR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJawhoAASnyhQR0LABvJj-zOE'
+    }
+  },
+  {
+    name: 'Bursa',
+    company: 'careem',
+    info: {
+      formatted_address: '60 W Portal Ave, San Francisco, CA 94127, USA',
+      country: {
+        long_name: 'United States',
+        short_name: 'US',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ3d4n-Op9j4AR7r-nYVNINr0'
+    }
+  },
+  {
+    name: 'Adana',
+    company: 'careem',
+    info: {
+      formatted_address: '1449 E Pine St, Seattle, WA 98122, USA',
+      country: {
+        long_name: 'United States',
+        short_name: 'US',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJXTZWDtJqkFQRYn1D802VvkI'
+    }
+  },
+  {
+    name: 'Khartoum',
+    company: 'careem',
+    info: {
+      formatted_address: 'Khartoum, Sudan',
+      country: {
+        long_name: 'Sudan',
+        short_name: 'SD',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJv8o3mN6PjhYRQNtnft5VHxk'
+    }
+  },
+  {
+    name: 'Peshawar',
+    company: 'careem',
+    info: {
+      formatted_address: 'Peshawar, Khyber Pakhtunkhwa, Pakistan',
+      country: {
+        long_name: 'Pakistan',
+        short_name: 'PK',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJz3kOD7kX2TgREqRYdWOyFqg'
+    }
+  },
+  {
+    name: 'Multan',
+    company: 'careem',
+    info: {
+      formatted_address: 'Multan, Punjab, Pakistan',
+      country: {
+        long_name: 'Pakistan',
+        short_name: 'PK',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ2afeeFcxOzkRL9RVTscv17o'
+    }
+  },
+  {
+    name: 'Ismailia',
+    company: 'careem',
+    info: {
+      formatted_address: 'Ismailia, Ismailia Governorate, Egypt',
+      country: {
+        long_name: 'Egypt',
+        short_name: 'EG',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJuRAeGVZZ-BQRlVsPW3czCTs'
+    }
+  },
+  {
+    name: 'Ramallah',
+    company: 'careem',
+    info: {
+      formatted_address: 'Ramallah',
+      place_id: 'ChIJ0Vgt2kzVAhURdiyLzBdNbb8'
+    }
+  },
+  {
+    name: 'Gaza',
+    company: 'careem',
+    info: {
+      formatted_address: 'Gaza Strip',
+      place_id: 'ChIJqViyBEGE_RQR5-hLGUux3P0'
+    }
+  },
+  {
+    name: 'Nablus',
+    company: 'careem',
+    info: {
+      formatted_address: 'Nablus',
+      place_id: 'ChIJl1ZCUPbgHBURhD0VvTCpC38'
+    }
+  },
+  {
+    name: 'Baghdad',
+    company: 'careem',
+    info: {
+      formatted_address: 'Baghdad, Iraq',
+      country: {
+        long_name: 'Iraq',
+        short_name: 'IQ',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJk0GnoGd_VxURLG-xo9Kp7Z0'
+    }
+  }
+]
