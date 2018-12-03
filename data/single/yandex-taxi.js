60be59f1c0c5585520978425ed76bff50c6bf978
Sara Vieira <hey@iamsaravieira.com>
2018-12-03 09:02:34 +0100

    add yandex

---
+++ data/single/yandex-taxi.js
@@ -0,0 +1,223 @@
+module.exports = [
+  {
+    name: 'Saint Petersburg',
+    company: 'Yandex Taxi',
+    info: {
+      formatted_address: 'St Petersburg, Russia',
+      country: {
+        long_name: 'Russia',
+        short_name: 'RU',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ7WVKx4w3lkYR_46Eqz9nx20'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Kazan',
+    info: {
+      formatted_address: 'Kazan, Tatarstan, Russia',
+      country: {
+        long_name: 'Russia',
+        short_name: 'RU',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJmc2sfCutXkERZYyttbl3y38'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Novosibirsk',
+    info: {
+      formatted_address: 'Novosibirsk, Novosibirsk Oblast, Russia',
+      country: {
+        long_name: 'Russia',
+        short_name: 'RU',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJl03MkOHl30IRhenT4XMGOps'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Yekaterinburg',
+    info: {
+      formatted_address: 'Yekaterinburg, Sverdlovsk Oblast, Russia',
+      country: {
+        long_name: 'Russia',
+        short_name: 'RU',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJS9tioOplwUMRIH9W99dDAtU'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Nizhny Novgorod',
+    info: {
+      formatted_address: 'Nizhny Novgorod, Nizhny Novgorod Oblast, Russia',
+      country: {
+        long_name: 'Russia',
+        short_name: 'RU',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJUTID7vnVUUERqleWhM_A0yg'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Ufa',
+    info: {
+      formatted_address: 'Ufa, Republic of Bashkortostan, Russia',
+      country: {
+        long_name: 'Russia',
+        short_name: 'RU',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ4bwWkiU62UMRzJLaHpLA7qw'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Minsk',
+    info: {
+      formatted_address: 'Minsk, Belarus',
+      country: {
+        long_name: 'Belarus',
+        short_name: 'BY',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ02oeW9PP20YR2XC13VO4YQs'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Tallin',
+    info: {
+      formatted_address: 'Tallinn, Estonia',
+      country: {
+        long_name: 'Estonia',
+        short_name: 'EE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJvxZW35mUkkYRcGL8GG2zAAQ'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Riga',
+    info: {
+      formatted_address: 'Riga, Latvia',
+      country: {
+        long_name: 'Latvia',
+        short_name: 'LV',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ7T0H5bDP7kYRMP7yaM3PAAQ'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Chisinau',
+    info: {
+      formatted_address: 'Chisinau, Moldova',
+      country: {
+        long_name: 'Moldova',
+        short_name: 'MD',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJoWm3KDZ8yUARPN1JVzDW0Tc'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Almaty',
+    info: {
+      formatted_address: 'Almaty, Kazakhstan',
+      country: {
+        long_name: 'Kazakhstan',
+        short_name: 'KZ',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJq8vFFn1ugzgRdm2YrY9mRD0'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Yerevan',
+    info: {
+      formatted_address: 'Yerevan, Armenia',
+      country: {
+        long_name: 'Armenia',
+        short_name: 'AM',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJW4v8uNqiakARalLah655FD0'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Vanadzor',
+    info: {
+      formatted_address: 'Vanadzor, Armenia',
+      country: {
+        long_name: 'Armenia',
+        short_name: 'AM',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJV-ypwhy8QUAR9H8MGcbw5DQ'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Gyumri',
+    info: {
+      formatted_address: 'Gyumri, Armenia',
+      country: {
+        long_name: 'Armenia',
+        short_name: 'AM',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJn_w0m4v7QUARApH5RQ7tS08'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Tbilisi',
+    info: {
+      formatted_address: 'Tbilisi, Georgia',
+      country: {
+        long_name: 'Georgia',
+        short_name: 'GE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJa2JP5tcMREARo25X4u2E0GE'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Tashkent',
+    info: {
+      formatted_address: 'Tashkent, Uzbekistan',
+      country: {
+        long_name: 'Uzbekistan',
+        short_name: 'UZ',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJw-l5wwyLrjgRmMulSjsyqaU'
+    }
+  },
+  {
+    company: 'Yandex Taxi',
+    name: 'Belgrade',
+    info: {
+      formatted_address: 'Belgrade, Serbia',
+      country: {
+        long_name: 'Serbia',
+        short_name: 'RS',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJvT-116N6WkcR5H4X8lxkuB0'
+    }
+  }
+]
