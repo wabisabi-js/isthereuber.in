77b2742f33f4bd9922e129ea13429bea751aeb50
Ma7eer <maher.alkendi2@gmail.com>
2018-12-03 17:26:20 +0000

    added heetch

---
+++ data/single/heetch.js
@@ -0,0 +1,171 @@
+module.exports = [
+  {
+    name: 'Paris',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Paris, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJD7fiBh9u5kcRYJSMaMOCCwQ'
+    }
+  },
+  {
+    name: 'Lille',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Lille, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJEW4ls3nVwkcRYGNkgT7xCgQ'
+    }
+  },
+  {
+    name: 'Lyon',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Lyon, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJl4foalHq9EcR8CG75CqrCAQ'
+    }
+  },
+  {
+    name: 'Nice',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Nice, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJMS2FahDQzRIRcJqX_aUZCAQ'
+    }
+  },
+  {
+    name: 'Bordeaux',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Bordeaux, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJgcpR9-gnVQ0RiXo5ewOGY3k'
+    }
+  },
+  {
+    name: 'Nantes',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Nantes, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJra6o8IHuBUgRMO0NHlI3DQQ'
+    }
+  },
+  {
+    name: 'Toulouse',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Toulouse, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ_1J17G-7rhIRMBBBL5z2BgQ'
+    }
+  },
+  {
+    name: 'Strasbourg',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Strasbourg, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJwbIYXknIlkcRHyTnGDFIGpc'
+    }
+  },
+  {
+    name: 'Marseille',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Marseille, France',
+      country: {
+        long_name: 'France',
+        short_name: 'FR',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJM1PaREO_yRIRIAKX_aUZCAQ'
+    }
+  },
+  {
+    name: 'Brussels',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Brussels, Belgium',
+      country: {
+        long_name: 'Belgium',
+        short_name: 'BE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ_58PdIbEw0cRMIBML6uZAAE'
+    }
+  },
+  {
+    name: 'kite',
+    company: 'heetch',
+    info: {
+      formatted_address: '550 N Rock Rd #60, Wichita, KS 67206, USA',
+      country: {
+        long_name: 'United States',
+        short_name: 'US',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJGQdR5TbjuocRNCeQWblyfn0'
+    }
+  },
+  {
+    name: 'Stockholm',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Stockholm, Sweden',
+      country: {
+        long_name: 'Sweden',
+        short_name: 'SE',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJywtkGTF2X0YRZnedZ9MnDag'
+    }
+  },
+  {
+    name: 'Casablanca',
+    company: 'heetch',
+    info: {
+      formatted_address: 'Casablanca, Morocco',
+      country: {
+        long_name: 'Morocco',
+        short_name: 'MA',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJOxGqeEfNpw0R0w8xT9jBBgs'
+    }
+  }
+]
