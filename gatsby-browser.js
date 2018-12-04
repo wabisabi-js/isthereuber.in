7157f3141cfde3ed155c89155bee094422bff2e4
Sara Vieira <hey@iamsaravieira.com>
2018-12-04 09:47:23 +0100

    add fix weird spaces issues

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,18 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+// fix for when cities have spaces
 
-// You can delete this file if you're not using it
+exports.onPrefetchPathname = () => {
+  const path = location.pathname
+  if (!path.includes('search')) {
+    const cleanURL = decodeURI(path)
+      .split(' ')
+      .join('-')
+      .toLowerCase()
+      .trim()
+
+    if (cleanURL !== path) {
+      window.history.pushState('profile', null, '/')
+      history.pushState(null, null, cleanURL)
+      window.location.reload()
+    }
+  }
+}
