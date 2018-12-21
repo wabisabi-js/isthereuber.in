02cd82705e565bed52df453be7bb8a0495444fc6
Zhigang Fang <zhigang1992@gmail.com>
2018-12-21 19:34:24 +0800

    Providers in China, didi and caocao (#78)

---
+++ data/single/caocao.js
@@ -0,0 +1,327 @@
+module.exports = [
+  {
+    name: 'Hangzhou',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Hangzhou, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJmaqaQym2SzQROuhNgoPRv6c'
+    }
+  },
+  {
+    name: 'Beijing',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Beijing, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJuSwU55ZS8DURiqkPryBWYrk'
+    }
+  },
+  {
+    name: 'Shanghai',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Shanghai, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJMzz1sUBwsjURoWTDI5QSlQI'
+    }
+  },
+  {
+    name: 'Guangzhou',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Guangzhou, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJxytco5X4AjQRFeTqrXXgWQ4'
+    }
+  },
+  {
+    name: 'Shenzhen',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Shenzhen, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJkVLh0Aj0AzQRyYCStw1V7v0'
+    }
+  },
+  {
+    name: 'Tianjin',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Tianjin, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJhw4sHmL87TURMUCIMLeUcYQ'
+    }
+  },
+  {
+    name: 'Chongqing',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Chongqing, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJQ0_m87o0kzYRIbOI3BaGn94'
+    }
+  },
+  {
+    name: 'Nanjing',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Nanjing, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJg82NZpuMtTURBhvfeQu2-48'
+    }
+  },
+  {
+    name: 'Chengdu',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Chengdu, Sichuan, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJIXdEACPF7zYRAg4kLs5Shrk'
+    }
+  },
+  {
+    name: 'Wuhan',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Wuhan, Hubei, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJb_KF3fiuLjQRECJYbKzJwjk'
+    }
+  },
+  {
+    name: 'Changsha',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Changsha, Hunan, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJxWQcnvM1JzQRgKbxoZy75bE'
+    }
+  },
+  {
+    name: 'Harbin',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Harbin, Heilongjiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJYRRkpvhkQ14R1SygWnOSfF4'
+    }
+  },
+  {
+    name: 'Xi',
+    company: 'caocao',
+    info: {
+      formatted_address: "Xi'an, Shaanxi, China",
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJuResIul5YzYRLliUp_1m1IU'
+    }
+  },
+  {
+    name: 'Lanzhou',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Lanzhou, Gansu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJgzkMmbWQWjYRXCQNXS4ATJE'
+    }
+  },
+  {
+    name: 'Taiyuan',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Taiyuan, Shanxi, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJAZbireuoCjYRKz3lehMyvdo'
+    }
+  },
+  {
+    name: 'Xiamen',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Xiamen, Fujian, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJJ-u_5XmDFDQRVtBolgpnoCg'
+    }
+  },
+  {
+    name: 'Ningbo',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Ningbo, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ91gIY1RjTTQRc_HMRvgjh5Q'
+    }
+  },
+  {
+    name: 'Dalian',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Dalian, Liaoning, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJP1j2OhRahjURNsllbOuKc3Y'
+    }
+  },
+  {
+    name: 'Qingdao',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Qingdao, Shandong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJa_D4gtUPljUR8_JMYfqCTWE'
+    }
+  },
+  {
+    name: 'Suzhou',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Suzhou, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJB17Sm9GgszURBGB2vYV_5SE'
+    }
+  },
+  {
+    name: 'Yingkou',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Yingkou, Liaoning, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJn8W47tE3KF4RXqVvY_BU9pI'
+    }
+  },
+  {
+    name: 'Jinhua',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Jinhua, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJv8wKpTQSSTQRYT_F7ERhMlM'
+    }
+  },
+  {
+    name: 'Foshan',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Foshan, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJt6V5UFhZAjQRGe397NrIYUA'
+    }
+  },
+  {
+    name: 'Wuxi',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Wuxi, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJpabgQ1iOszURJjX9xBftNtU'
+    }
+  },
+  {
+    name: 'Changde',
+    company: 'caocao',
+    info: {
+      formatted_address: 'Changde, Hunan, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJL9wOEXZXnDYR5A8DlE6kNTE'
+    }
+  }
+]
---
+++ data/single/didi.js
@@ -0,0 +1,561 @@
+module.exports = [
+  {
+    name: 'Beijing',
+    company: 'didi',
+    info: {
+      formatted_address: 'Beijing, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJuSwU55ZS8DURiqkPryBWYrk'
+    }
+  },
+  {
+    name: 'Guangzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Guangzhou, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJxytco5X4AjQRFeTqrXXgWQ4'
+    }
+  },
+  {
+    name: 'Hangzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Hangzhou, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJmaqaQym2SzQROuhNgoPRv6c'
+    }
+  },
+  {
+    name: 'Shenzhen',
+    company: 'didi',
+    info: {
+      formatted_address: 'Shenzhen, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJkVLh0Aj0AzQRyYCStw1V7v0'
+    }
+  },
+  {
+    name: 'Chengdu',
+    company: 'didi',
+    info: {
+      formatted_address: 'Chengdu, Sichuan, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJIXdEACPF7zYRAg4kLs5Shrk'
+    }
+  },
+  {
+    name: 'Wuhan',
+    company: 'didi',
+    info: {
+      formatted_address: 'Wuhan, Hubei, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJb_KF3fiuLjQRECJYbKzJwjk'
+    }
+  },
+  {
+    name: 'Tianjin',
+    company: 'didi',
+    info: {
+      formatted_address: 'Tianjin, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJhw4sHmL87TURMUCIMLeUcYQ'
+    }
+  },
+  {
+    name: 'Chongqing',
+    company: 'didi',
+    info: {
+      formatted_address: 'Chongqing, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJQ0_m87o0kzYRIbOI3BaGn94'
+    }
+  },
+  {
+    name: 'Shaoxing',
+    company: 'didi',
+    info: {
+      formatted_address: 'Shaoxing, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ2wsVuvlCTDQRzt25UOHtWRg'
+    }
+  },
+  {
+    name: 'Qingdao',
+    company: 'didi',
+    info: {
+      formatted_address: 'Qingdao, Shandong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJa_D4gtUPljUR8_JMYfqCTWE'
+    }
+  },
+  {
+    name: 'Jiaxing',
+    company: 'didi',
+    info: {
+      formatted_address: 'Jiaxing, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJoZwNwgglszURnK1eqfG8F04'
+    }
+  },
+  {
+    name: 'Huzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Huzhou, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJBbct-WhLSzQR8PQStBNwRjQ'
+    }
+  },
+  {
+    name: 'Suzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Suzhou, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJB17Sm9GgszURBGB2vYV_5SE'
+    }
+  },
+  {
+    name: 'Changsha',
+    company: 'didi',
+    info: {
+      formatted_address: 'Changsha, Hunan, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJxWQcnvM1JzQRgKbxoZy75bE'
+    }
+  },
+  {
+    name: 'Nanjing',
+    company: 'didi',
+    info: {
+      formatted_address: 'Nanjing, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJg82NZpuMtTURBhvfeQu2-48'
+    }
+  },
+  {
+    name: 'Hefei',
+    company: 'didi',
+    info: {
+      formatted_address: 'Hefei, Anhui, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJnc8H8g5kyzURmTLD8nMRFdw'
+    }
+  },
+  {
+    name: 'Jinan',
+    company: 'didi',
+    info: {
+      formatted_address: 'Jinan, Shandong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJE-qsHbkFwjURVEjOPZZQMGE'
+    }
+  },
+  {
+    name: 'Fuzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Fuzhou, Fujian, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJQZ8yJz5RQDQRiIspSgmF8m4'
+    }
+  },
+  {
+    name: 'Nanning',
+    company: 'didi',
+    info: {
+      formatted_address: 'Nanning, Guangxi, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJefRg2ylhsTYRsr_pIoJJUoA'
+    }
+  },
+  {
+    name: 'Xiamen',
+    company: 'didi',
+    info: {
+      formatted_address: 'Xiamen, Fujian, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJJ-u_5XmDFDQRVtBolgpnoCg'
+    }
+  },
+  {
+    name: 'Ningbo',
+    company: 'didi',
+    info: {
+      formatted_address: 'Ningbo, Zhejiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ91gIY1RjTTQRc_HMRvgjh5Q'
+    }
+  },
+  {
+    name: 'Quanzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Quanzhou, Fujian, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJFVc-HyvUFDQRCbEIsguuNUM'
+    }
+  },
+  {
+    name: 'Xi',
+    company: 'didi',
+    info: {
+      formatted_address: "Xi'an, Shaanxi, China",
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJuResIul5YzYRLliUp_1m1IU'
+    }
+  },
+  {
+    name: 'Shenyang',
+    company: 'didi',
+    info: {
+      formatted_address: 'Shenyang, Liaoning, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJtzQTOkE8JV4RZpGSzvDkmVQ'
+    }
+  },
+  {
+    name: 'Huizhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Huizhou, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJDWb7zyWOBDQRB9vl0zFCCm8'
+    }
+  },
+  {
+    name: 'Kunming',
+    company: 'didi',
+    info: {
+      formatted_address: 'Kunming, Yunnan, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJy9MnEsOD0DYRNgpPmKTzscw'
+    }
+  },
+  {
+    name: 'Dongguan',
+    company: 'didi',
+    info: {
+      formatted_address: 'Dongguan, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ5ROI3ZQKAzQRQ3q8DjAgg3Y'
+    }
+  },
+  {
+    name: 'Yantai',
+    company: 'didi',
+    info: {
+      formatted_address: 'Yantai, Shandong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJxZVL14RjkDUR1XRgDdZJxk4'
+    }
+  },
+  {
+    name: 'Zhuhai',
+    company: 'didi',
+    info: {
+      formatted_address: 'Zhuhai, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJta-eZoVoATQRBMbqOlLiMd8'
+    }
+  },
+  {
+    name: 'Sanya',
+    company: 'didi',
+    info: {
+      formatted_address: 'Sanya, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ_ae1SZdLRTERFyEkKNKYXCs'
+    }
+  },
+  {
+    name: 'Zhongshan',
+    company: 'didi',
+    info: {
+      formatted_address: 'Zhongshan, Guangdong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ5dch_DctAjQRyRe9y4rWbLQ'
+    }
+  },
+  {
+    name: 'Linyi',
+    company: 'didi',
+    info: {
+      formatted_address: 'Linyi, Shandong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJyenY4ue3wDURNG4jIzOZlzw'
+    }
+  },
+  {
+    name: 'Dalian',
+    company: 'didi',
+    info: {
+      formatted_address: 'Dalian, Liaoning, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJP1j2OhRahjURNsllbOuKc3Y'
+    }
+  },
+  {
+    name: 'Taiyuan',
+    company: 'didi',
+    info: {
+      formatted_address: 'Taiyuan, Shanxi, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJAZbireuoCjYRKz3lehMyvdo'
+    }
+  },
+  {
+    name: 'Tangshan',
+    company: 'didi',
+    info: {
+      formatted_address: 'Tangshan, Hebei, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ8Vn-fBLy8jURoH06lu3qza4'
+    }
+  },
+  {
+    name: 'Harbin',
+    company: 'didi',
+    info: {
+      formatted_address: 'Harbin, Heilongjiang, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJYRRkpvhkQ14R1SygWnOSfF4'
+    }
+  },
+  {
+    name: 'Zhengzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Zhengzhou, Henan, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJnWqb8JRl1zURLO3M9BnuMnk'
+    }
+  },
+  {
+    name: 'Weihai',
+    company: 'didi',
+    info: {
+      formatted_address: 'Weihai, Shandong, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJReSBoopomjURmhZT7n6st3E'
+    }
+  },
+  {
+    name: 'Changzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Changzhou, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJ855yDcoStDURODxvgUoktMA'
+    }
+  },
+  {
+    name: 'Wuxi',
+    company: 'didi',
+    info: {
+      formatted_address: 'Wuxi, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJpabgQ1iOszURJjX9xBftNtU'
+    }
+  },
+  {
+    name: 'Yangzhou',
+    company: 'didi',
+    info: {
+      formatted_address: 'Yangzhou, Jiangsu, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJI2wyEV59tjUR-Fq08n2uqdI'
+    }
+  },
+  {
+    name: 'Shijiazhuang',
+    company: 'didi',
+    info: {
+      formatted_address: 'Shijiazhuang, Hebei, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJPQ5ioJjd5jURUA2kznO_WBw'
+    }
+  },
+  {
+    name: 'Changchun',
+    company: 'didi',
+    info: {
+      formatted_address: 'Changchun, Jilin, China',
+      country: {
+        long_name: 'China',
+        short_name: 'CN',
+        types: ['country', 'political']
+      },
+      place_id: 'ChIJb5q9bfNdOF4RmLFWJ_TUx58'
+    }
+  }
+]
